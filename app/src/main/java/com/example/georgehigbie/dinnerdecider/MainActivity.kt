57be3252026f98c356969ec6a56bb608b059afc9
George Higbie <gehigbie@gmail.com>
2017-09-10 19:28:35 -0600

    Add method to button

--- app/src/main/java/com/example/georgehigbie/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/georgehigbie/dinnerdecider/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.georgehigbie.dinnerdecider
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese", "Hamburger", "Pizza", "Burger King", "Valentinos")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        decideBtn.setOnClickListener{
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodText.text = foodList[randomFood]
+        }
     }
 }
